03edb70ebbb2d7e45d333e73078ba98022a65b3e
wuyong <biggodw@gmail.com>
2025-02-09 17:54:27 +0800

    添加电商后台

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ const liveUrls = {
   douyin: "https://live.douyin.com/215450079114",
   kuaishou: "https://live.kuaishou.com/u/3xgg5x5x6f4w6em",
   weixin: "https://channels.weixin.qq.com/platform/live/liveBuild",
-  taobao: "https://myseller.taobao.com/home.htm/qn-live-container/live/list",
+  taobao: "https://pages-fast.m.taobao.com/wow/z/app/tbpc/tbzb-anchor/index?id=2870654412",
 };
 
 const checkboxes = document.querySelectorAll('input[name="lives"]');
@@ -21,3 +21,8 @@ btn.addEventListener("click", () => {
     chrome.windows.create({ url:liveUrls[item.value], type: "normal", width: 350, left });
   });
 });
+
+const shopBtn = document.getElementById('shopurl')
+shopBtn.addEventListener('click',()=>{
+  chrome.windows.create({url:"https://live.wubug.cn/live", type: "normal"})
+})
